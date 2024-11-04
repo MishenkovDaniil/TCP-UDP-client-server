3d250a2d928dc669bec645631aa2e32e213f93f3
Mishenkov Daniil <mishenkovdaniil@mail.ru>
2024-11-04 07:41:40 +0300

    add client-server loop

--- tcp/tcp_client.cpp
+++ tcp/tcp_client.cpp
@@ -8,6 +8,71 @@
 #include "tcp.hpp"
 #include <unistd.h>
 
+const int ERROR_CODE = -1;
+
+int init_tsp_client (int port) {
+    int socket_fd = tcp::create_socket ();
+    if (socket_fd < 0) {
+        return ERROR_CODE;
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    
+    int connect_status = tcp::connect_to_server (socket_fd, (const sockaddr *)&addr, sizeof (addr));
+    if (connect_status) {
+        perror ("connect");
+        return ERROR_CODE;
+    }
+
+    return socket_fd;
+}
+
+int run_client_loop (int server_fd) {
+    char msg [1024] = "";
+    
+    int n_read_bytes = 0;
+    int n_written_bytes = 0;
+
+    while (1) {
+        printf ("> ");
+        fflush (stdout);
+
+        int input_size = read(STDIN_FILENO, msg, 1024);
+        if (input_size < 0) {
+            fprintf (stderr, "No input.\n");
+            return -1;
+        } 
+
+        msg[input_size - 1] = '\0'; // rm '\n' for strcmp 
+
+        if (!strcmp(msg, "exit")) {
+            tcp::send_to(server_fd, (void*)msg, input_size, 0);
+            printf ("bye-bye!\n");
+            break;
+        }
+
+        n_written_bytes += tcp::send_to(server_fd, (void*)msg, input_size, 0);
+        
+        printf ("Awaiting server response...\n");
+        
+        memset(&msg, 0, sizeof(msg));
+        
+        n_read_bytes += tcp::recv_from(server_fd, (void*)msg, sizeof(msg), 0);
+        
+        if(!strcmp(msg, "exit")) {
+            printf ("Server has quit the session.\n");
+            break;
+        }
+
+        printf ("Server: %s\n", msg);
+        memset(&msg, 0, sizeof(msg));
+    }
+    return 0;
+}
+
 int main(int argc, char **argv){
     if (argc != 2) {
         printf("Usage: %s <port>\n", argv[0]);
@@ -15,30 +80,23 @@ int main(int argc, char **argv){
     }
 
     int port = atoi(argv[1]);
-    int socket_fd = tcp::create_socket ();
-
-    char buffer[1024];
-
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = INADDR_ANY; // inet_addr (ip)
     
-    int connect_status = tcp::connect_to_server (socket_fd, (const sockaddr *)&addr, sizeof (addr));
-    if (connect_status) {
-        perror ("connect");
-        return -1;
+    int socket_fd = init_tsp_client (port);
+    if (socket_fd == ERROR_CODE) {
+        return ERROR_CODE;
     }
 
-    bzero(buffer, 1024);
-    strcpy(buffer, "Hello, World!");
-    int bytes_num = tcp::send_to (socket_fd, buffer, sizeof(buffer), 0);
-    printf("[+]Data send %d: %s\n",bytes_num, buffer);
+    run_client_loop (socket_fd);
+    // char buffer[1024] = "";
 
-    bzero(buffer, 1024);
-    socklen_t addr_size = sizeof(addr);
-    tcp::recv_from(socket_fd, buffer, sizeof(buffer), 0);
-    printf("[+]Data recv: %s\n", buffer);
+    // bzero(buffer, 1024);
+    // strcpy(buffer, "Hello, World!");
+    // int bytes_num = tcp::send_to (socket_fd, buffer, sizeof(buffer), 0);
+    // printf("[+]Data send %d: %s\n",bytes_num, buffer);
+
+    // bzero(buffer, 1024);
+    // tcp::recv_from(socket_fd, buffer, sizeof(buffer), 0);
+    // printf("[+]Data recv: %s\n", buffer);
     
     close (socket_fd);
 
--- tcp/tcp_server.cpp
+++ tcp/tcp_server.cpp
@@ -10,37 +10,86 @@
 
 #include "tcp.hpp"
 
+const int ERROR_CODE = -1;
+
+int init_tsp_server (int port) {
+    int socket_fd = tcp::create_socket ();
+    if (socket_fd == -1) {
+        perror("create socket error");
+        return ERROR_CODE;
+    }
+
+    int bind_status = tcp::bind_socket (socket_fd, port);
+    if (bind_status) {
+        perror ("bind error:");
+        return ERROR_CODE;
+    }
+
+    int listen_status = tcp::listen_on_socket (socket_fd);
+    if (listen_status < 0) {
+        perror ("listen error");
+        return ERROR_CODE;
+    }
+
+    return socket_fd;
+}
+
+int run_server_loop (int client_fd) {
+    char msg [1024] = "";
+    
+    int n_read_bytes = 0;
+    int n_written_bytes = 0;
+
+    while (1) {
+        printf ("Awaiting client msg...\n");
+        n_read_bytes += tcp::recv_from(client_fd, (void*)msg, sizeof(msg), 0);
+        
+        if(!strcmp(msg, "exit")) {
+            printf ("client quit the session.\n");
+            break;
+        }
+
+        printf ("client %d: %s\n>", client_fd, msg);
+        fflush (stdout);
+        
+        memset(&msg, 0, sizeof(msg));
+
+        int input_size = read(STDIN_FILENO, msg, 1024);
+        if (input_size < 0) {
+            fprintf (stderr, "No input.\n");
+            return -1;
+        } 
+
+        msg[input_size - 1] = '\0'; // rm '\n' for strcmp 
+        
+        if (!strcmp(msg, "exit")) {
+            tcp::send_to(client_fd, (void*)msg, input_size, 0);
+            printf ("bye-bye!\n");
+            break;
+        }
+
+        n_written_bytes += tcp::send_to(client_fd, (void*)msg, input_size, 0);
+        memset(&msg, 0, sizeof(msg));
+    }
+    return 0;
+}
 
 int main(int argc, char **argv){
     if (argc != 2){
         printf("Usage: %s <port>\n", argv[0]);
-        return -1;
+        return ERROR_CODE;
     }
 
     char *ip = "127.0.0.1";
     int port = atoi(argv[1]);
     
-    int socket_fd = tcp::create_socket ();
-    if (socket_fd == -1) {
-        perror("[-]socket error");
-        return -1;
+    int socket_fd = init_tsp_server (port);
+    if (socket_fd == ERROR_CODE) {
+        return ERROR_CODE;
     }
 
-    int bind_status = tcp::bind_socket (socket_fd, port);
-    if (bind_status) {
-        perror ("Bind error:");
-        return -1;
-    }
+    // char buffer[1024]= "";
 
-    int listen_status = tcp::listen_on_socket (socket_fd);
-    if (listen_status < 0) {
-        perror ("listen");
-        return -1;
-    }
-
-    char buffer[1024];
-
-    bzero(buffer, 1024);
     struct sockaddr_in client_addr;
     memset(&client_addr, '\0', sizeof(client_addr));
     socklen_t client_addr_size = sizeof(client_addr);
@@ -48,13 +97,15 @@ int main(int argc, char **argv){
     int client_socket_fd = tcp::accept_client (socket_fd, NULL, NULL);
     printf ("%d\n", client_socket_fd);
 
-    int bytes_recv = tcp::recv_from (client_socket_fd, buffer, sizeof(buffer), 0);
-    printf("[+]Data recv %d: %s\n",bytes_recv, buffer);
-    bzero(buffer, 1024);
+    run_server_loop (client_socket_fd);
 
-    strcpy(buffer, "Welcome");
-    tcp::send_to(client_socket_fd, buffer, sizeof(buffer), 0);
-    printf("[+]Data send: %s\n", buffer);
+    // int bytes_recv = tcp::recv_from (client_socket_fd, buffer, sizeof(buffer), 0);
+    // printf("[+]Data recv %d: %s\n",bytes_recv, buffer);
+    // bzero(buffer, 1024);
+
+    // strcpy(buffer, "Welcome");
+    // tcp::send_to(client_socket_fd, buffer, sizeof(buffer), 0);
+    // printf("[+]Data send: %s\n", buffer);
 
     close (socket_fd);
 
